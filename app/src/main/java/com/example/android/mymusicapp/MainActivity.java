f68946ad0d374176be42072ccca342c273674907
AiGa <aiga.andrejeva@gmail.com>
2017-09-01 03:05:30 +0100

    First commit

---
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.android.mymusicapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView playlists = (TextView) findViewById(R.id.playlists);
+
+        // Set a click listener on that View
+        playlists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the playlists category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link PlaylistsActivity}
+                Intent playlistsIntent = new Intent(MainActivity.this, PlaylistsActivity.class);
+
+                // Start the new activity
+                startActivity(playlistsIntent);
+            }
+        });
+        TextView albums = (TextView) findViewById(R.id.albums);
+
+        // Set a click listener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the albums category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link AlbumsActivity}
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+
+                // Start the new activity
+                startActivity(albumsIntent);
+            }
+        });
+        TextView artists = (TextView) findViewById(R.id.artists);
+
+        // Set a click listener on that View
+        artists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the artists category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ArtistsActivity}
+                Intent artistsIntent = new Intent(MainActivity.this, ArtistsActivity.class);
+
+                // Start the new activity
+                startActivity(artistsIntent);
+            }
+        });
+        TextView genres = (TextView) findViewById(R.id.genres);
+
+        // Set a click listener on that View
+        genres.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the genres category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link GenresActivity}
+                Intent genresIntent = new Intent(MainActivity.this, GenresActivity.class);
+
+                // Start the new activity
+                startActivity(genresIntent);
+            }
+        });
+        TextView songs = (TextView) findViewById(R.id.songs);
+
+        // Set a click listener on that View
+        songs.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the songs category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link SongsActivity}
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(songsIntent);
+            }
+        });
+
+        LinearLayout nowPlaying = (LinearLayout) findViewById(R.id.song_display);
+
+        //set click listener on that View
+        nowPlaying.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the nowPlaying category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NowPlayingActivity}
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+
+                // Start the new activity
+                startActivity(nowPlayingIntent);
+            }
+        });
+    }
+}
